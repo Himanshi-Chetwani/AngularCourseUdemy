292bfc228c95dfd0a3b3e4006233d1a9f7120eb5
Himanshi-Chetwani <himanshi.chetwani@gmail.com>
2021-05-26 13:22:22 -0500

    Assignment1

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning-alert.component.html',
+    styleUrls: ['./warning-alert.component.css']
+})
+
+export class WarningAlertComponent{
+
+}
